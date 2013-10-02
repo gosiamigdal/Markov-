ac8f651a2354c65e5dc3b280bc9830989d04d287
Hackbright Student <user@chromebox08.(none)>
2013-10-02 12:24:47 -0700

    Markov with twitter API(no passwords)

--- markov.py
+++ markov.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
 from sys import argv
-script, inputfile = argv
 import random
+import twitter
+
+
+def twitter_push(message):
+    api = twitter.Api()
+    status = api.PostUpdate(message)
+    print status.text
+
 
 
 def make_chains(corpus):
@@ -13,17 +20,15 @@ def make_chains(corpus):
 
     for index in xrange(len(corpus) - 2):
         key = (corpus[index], corpus[index + 1])
+        val = corpus[index + 2]
         if key not in markov_dictionary:
-            val = [corpus[index + 2]]
-            markov_dictionary[key] = val
+            markov_dictionary[key] = [val]
         else:
             #longhand version:
             # mk_list = markov_dictionary[key]
             # mk_list.append(val)
             # markov_dictionary[key] = mk_list
-
-            #shorthand version:
-            markov_dictionary[key].append(corpus[index+1])       
+            markov_dictionary[key].append(val)       
     return markov_dictionary
         
 def change_tuple(tupl):
@@ -36,13 +41,17 @@ def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
     tweet_content = []
+    first_key = random.choice(chains.keys())
+    first_value = random.choice(chains[first_key])
+    tweet_content.append(first_key[0])
+    tweet_content.append(first_key[1])
+    tweet_content.append(first_value)
+    new_key_index = 1
     while len(" ".join(tweet_content)) <= 120:
-        tweet_key = random.choice(chains.keys())
-        tweet_value = random.choice(chains[tweet_key])
-        tweet_key = change_tuple(tweet_key)
-        tweet_content.append(tweet_key[0])
-        tweet_content.append(tweet_key[1])
-        tweet_content.append(tweet_value)
+        current_key = tweet_content[new_key_index], tweet_content[new_key_index + 1]
+        current_value = random.choice(chains[current_key])
+        tweet_content.append(current_value)
+        new_key_index += 1
 
     while len(" ".join(tweet_content)) >= 30:
         if "!" in tweet_content[-1]:
@@ -63,14 +72,12 @@ def make_text(chains):
 
 
 def main():
-    #args = sys.argv
-
-    # Change this to read input_text from a file
+    script, inputfile = argv
     input_text = open(inputfile).read()
-
     chain_dict = make_chains(input_text)
     random_text = make_text(chain_dict)
-    print random_text
+    twitter_push(random_text)
+
 
 if __name__ == "__main__":
     main()
